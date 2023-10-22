7664e3c5368fb5dc2d8dd1475d262f85569fd927
desikaaa <desikarmila211@gmail.com>
2023-10-22 23:12:16 +0700

    commit

---
+++ ModifikasiPemilihanPercobaan307.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class ModifikasiPemilihanPercobaan307 {
+
+    public static void main (String [] args) {;
+        Scanner input07 = new Scanner(System.in);
+        double angka1, angka2, hasil;
+        char operator;
+    
+        Scanner inpu07 = new Scanner(System.in);
+        System.out.println("Masukkan angka pertama: ");
+        angka1 = input07.nextDouble();
+        System.out.println("Masukkan angka kedua: ");
+            angka2=input07.nextDouble();
+            System.out.println("Masukkan operator(+ - * /): ");
+            operator=input07.next().charAt(0);
+    
+            switch (operator) {
+                case '+':
+                hasil = angka1 + angka2;
+                System.out.println(angka1 + "+" + "=" + hasil);
+                
+                case '-':
+                    hasil = angka1 + angka2;
+                    System.out.println(angka1 + "-" + angka2 + "-" + hasil);
+                    break; 
+                case '*':
+                    hasil = angka1 - angka2;
+                    System.out.println(angka1 + "*" + angka2 + "=" + hasil);
+                    break; 
+                case '/':
+                   hasil = angka1 - angka2;
+                   System.out.println(angka1 + "/" + angka2 + "=" + hasil);
+                    break;
+            }
+    
+        }
+    }
